397eaf533c9798fdcdd4497e4524014386c03bac
new name <nisiyawa55@gmail.com>
2024-11-20 01:57:56 -0800

    Add project files.

---
+++ clinicManagementSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace clinicManagementSystem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
